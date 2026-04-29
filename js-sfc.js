b6a31efaefe79168f667500e47cd5fc9ce912084
eduardonovaki <eduardo.novaki01@gmail.com>
2026-04-29 14:39:53 -0300

    adicionei animação para a navbar aparecer, comecei a organizar os dados que aparecerão ao clicar nos botoes, em JS

--- js-sfc.js
+++ js-sfc.js
@@ -1,6 +1,16 @@
 let navbar = document.getElementById("navbar");
+const titulos = {
+    libertadores: {
+        nome: "Copa Libertadores da America",
+        anos: [1962, 1963, 2011]
+    },
+    mundial: {
+        nome: "Mundial de Clubes",
+        anos: [1962, 1963]
+    }
+}
 window.addEventListener('scroll', function(){
-if (window.scrollY > 50) { // lembra de testar valores maiores depois
+if (window.scrollY > 500) { 
   navbar.classList.add('scrolled')
 } else {
   navbar.classList.remove('scrolled')
