13f538fcabe7ab3d494368f1d441330f9bd55ceb
eduardonovaki <eduardo.novaki01@gmail.com>
2026-04-30 16:21:32 -0300

    add de um listener pra click nos botoes dos titulos, ainda não abre, mas no console ja computa os clicks

--- js-sfc.js
+++ js-sfc.js
@@ -1,4 +1,5 @@
 let navbar = document.getElementById("navbar");
+let botoes = document.querySelectorAll(".trofeu-btn");
 const titulos = {
     libertadores: {
         nome: "Copa Libertadores da America",
@@ -7,6 +8,14 @@ const titulos = {
     mundial: {
         nome: "Mundial de Clubes",
         anos: [1962, 1963]
+    },
+    brasileirao: {
+      nome: "Brasileirao",
+      anos: [1961, 1962, 1963, 1964, 1965, 1968, 2002, 2004]
+    },
+    copa: {
+      nome: "Copa do Brasil",
+      anos: [2010]
     }
 }
 window.addEventListener('scroll', function(){
@@ -16,3 +25,10 @@ if (window.scrollY > 500) {
   navbar.classList.remove('scrolled')
 }
 })
+
+for (let botao of botoes) {
+  botao.addEventListener('click', function(){
+    let chave = botao.dataset.key;
+      console.log(chave);
+  })
+}
