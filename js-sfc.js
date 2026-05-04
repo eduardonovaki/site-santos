d78ed56cd6a21ebe6f71f280ea3e159c7560f89d
eduardonovaki <eduardo.novaki01@gmail.com>
2026-05-04 19:12:19 -0300

    adicionei coisas nos 3 arquivos, função para a imagem do botao clicado aparecer na tela + css no painel que surge. Ainda não ta bom, o botão não fica no topo do painel. Em html adicionei uma <img> para colocar dentro do painel

--- js-sfc.js
+++ js-sfc.js
@@ -1,20 +1,25 @@
 let navbar = document.getElementById("navbar");
 let botoes = document.querySelectorAll(".trofeu-btn");
+let painel = document.getElementById('timeline-drawer')
 const titulos = {
     libertadores: {
         nome: "Copa Libertadores da America",
+        img: "./img/liberta2.png",
         anos: [1962, 1963, 2011]
     },
     mundial: {
         nome: "Mundial de Clubes",
+        img: "./img/mundialpele.png",
         anos: [1962, 1963]
     },
     brasileirao: {
       nome: "Brasileirao",
+      img: "./img/brasil2004",
       anos: [1961, 1962, 1963, 1964, 1965, 1968, 2002, 2004]
     },
     copa: {
       nome: "Copa do Brasil",
+      img: "./img/copadobr",
       anos: [2010]
     }
 }
@@ -30,5 +35,16 @@ for (let botao of botoes) {
   botao.addEventListener('click', function(){
     let chave = botao.dataset.key;
       console.log(chave);
+    let dados = titulos[chave];
+      console.log(dados);
+    painel.style.display = 'block';
+    let imagemPainel = document.getElementById("timeline-trofeu-img");
+    imagemPainel.src = dados.img;
+    let textoPainel = document.getElementById("timeline-drawer-title");
+    textoPainel.textContent = dados.nome;
+    let botaoClose = document.getElementById("timeline-close");
+    botaoClose.addEventListener('click', function(){
+      painel.style.display = 'none';
+    })
   })
 }
