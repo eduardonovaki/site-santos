8da2a9c4d3d59ac6dc3b85901b1dd05ec0da97bf
eduardonovaki <eduardo.novaki01@gmail.com>
2026-05-07 00:04:43 -0300

    site finalizado! arrumei o site pro mobile, tudo lindo.

--- js-sfc.js
+++ js-sfc.js
@@ -7,38 +7,38 @@ const titulos = {
         nome: "Copa Libertadores da America",
         img: "./img/liberta2.png",
         conquistas: [
-          {ano:1962, mvp: "Melhor jogador: Pelé"},
-          {ano:1963, mvp: "Melhor jogador: Pelé"},
-          {ano:2011, mvp: "Melhor jogador: Neymar Jr"},
+          {ano:1962, mvp: "Melhor jogador: Pelé", rival:"Vice: Peñarol", placar:"Placar agregado: 5x4"},
+          {ano:1963, mvp: "Melhor jogador: Pelé", rival:"Vice: Boca Juniors", placar:"Placar agregado: 5x3"},
+          {ano:2011, mvp: "Melhor jogador: Neymar Jr", rival:"Vice: Peñarol", placar:"Placar agregado: 2x1"},
         ]
     },
     mundial: {
         nome: "Mundial de Clubes",
         img: "./img/mundialpele.png",
         conquistas:  [
-          {ano:1962, mvp: "Melhor jogador: Pelé"},
-          {ano:1963, mvp: "Melhor jogador: Pelé"},
+          {ano:1962, mvp: "Melhor jogador: Pelé", rival:"Vice: Benfica", placar:"Placar agregado: 8x4"},
+          {ano:1963, mvp: "Melhor jogador: Pelé", rival:"Vice: Milan", placar:"Placar agregado: 7x6"},
         ]
     },
     brasileirao: {
       nome: "Brasileirao",
       img: "./img/brasil2004.png",
       conquistas: [
-        {ano:1961, mvp: "Melhor jogador: Pelé"},
-        {ano:1962, mvp: "Melhor jogador: Pelé"},
-        {ano:1963, mvp: "Melhor jogador: Pelé"},
-        {ano:1964, mvp: "Melhor jogador: Pelé"},
-        {ano:1965, mvp: "Melhor jogador: Pelé"},
-        {ano:1968, mvp: "Melhor jogador: Pelé"},
-        {ano:2002, mvp: "Melhor jogador: Robinho"},
-        {ano:2004, mvp: "Melhor jogador: Diego Ribas"},
+        {ano:1961, mvp: "Melhor jogador: Pelé", rival:"Vice: Bahia", placar:"Placar agregado: 10x2"},
+        {ano:1962, mvp: "Melhor jogador: Pelé", rival:"Vice: Botafogo", placar:"Placar agregado: 6x3"},
+        {ano:1963, mvp: "Melhor jogador: Pelé", rival:"Vice: Bahia", placar:"Placar agregado: 8x0"},
+        {ano:1964, mvp: "Melhor jogador: Pelé", rival:"Vice: Flamengo", placar:"Placar agregado: 4x1"},
+        {ano:1965, mvp: "Melhor jogador: Pelé", rival:"Vice: Vasco da Gama", placar:"Placar agregado: 6x1"},
+        {ano:1968, mvp: "Melhor jogador: Pelé", rival:"Vice: Vasco da Gama", placar:"Placar agregado: 2x0"},
+        {ano:2002, mvp: "Melhor jogador: Robinho", rival:"Vice: Corinthians", placar:"Placar agregado: 5x2"},
+        {ano:2004, mvp: "Melhor jogador: Diego", rival:"Vice: Athletico Paranaense", placar:"Santos 89 pontos"},
       ]
     },
     copa: {
       nome: "Copa do Brasil",
       img: "./img/copadobr.png",
       conquistas: [
-        {ano:2010, mvp: "Melhor jogador: Neymar Jr"},
+      {ano:2010, mvp: "Melhor jogador: Neymar", rival:"Vice: Vitória", placar:"Placar agregado: 3x2"},
       ]
     }
 }
@@ -69,7 +69,9 @@ for (let botao of botoes) {
       let item = document.createElement('div');
       item.innerHTML = `
       <span class="conquista-ano">${conquistas.ano}</span>
-      <span class="conquista-mvp">${conquistas.mvp}</span> `
+      <span class="conquista-mvp">${conquistas.mvp}</span> 
+      <span class="conquista-rival">${conquistas.rival}</span>
+      <span class="conquista-placar">${conquistas.placar}</span>`
       dadosPainel.appendChild(item);
       item.classList.add("conquista-itemjs");
       window.addEventListener('scroll', function(){
