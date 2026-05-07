5ea12ac1b40279963de15df10b471081793fd760
eduardonovaki <eduardo.novaki01@gmail.com>
2026-05-06 23:03:27 -0300

    site praticamente finalizado, adicionei animação na scrollhint, alterei o texto na aba de titulos, mudei o tamanho da terceira 'janela' do site, adicionei mais informações na overlay das lendas na seção das lendas

--- js-sfc.js
+++ js-sfc.js
@@ -1,53 +1,56 @@
 let navbar = document.getElementById("navbar");
 let botoes = document.querySelectorAll(".trofeu-btn");
-let painel = document.getElementById('timeline-drawer')
+let painel = document.getElementById('timeline-drawer');
+let scrollHint = document.querySelector('.hero-scroll-hint');
 const titulos = {
     libertadores: {
         nome: "Copa Libertadores da America",
         img: "./img/liberta2.png",
         conquistas: [
-          {ano:1962, mvp: "Pelé"},
-          {ano:1963, mvp: "Pelé"},
-          {ano:2011, mvp: "Neymar Jr"},
+          {ano:1962, mvp: "Melhor jogador: Pelé"},
+          {ano:1963, mvp: "Melhor jogador: Pelé"},
+          {ano:2011, mvp: "Melhor jogador: Neymar Jr"},
         ]
     },
     mundial: {
         nome: "Mundial de Clubes",
         img: "./img/mundialpele.png",
         conquistas:  [
-          {ano:1962, mvp: "Pelé"},
-          {ano:1963, mvp: "Pelé"},
+          {ano:1962, mvp: "Melhor jogador: Pelé"},
+          {ano:1963, mvp: "Melhor jogador: Pelé"},
         ]
     },
     brasileirao: {
       nome: "Brasileirao",
       img: "./img/brasil2004.png",
       conquistas: [
-        {ano:1961, mvp: "Pelé"},
-        {ano:1962, mvp: "Pelé"},
-        {ano:1963, mvp: "Pelé"},
-        {ano:1964, mvp: "Pelé"},
-        {ano:1965, mvp: "Pelé"},
-        {ano:1968, mvp: "Pelé"},
-        {ano:2002, mvp: "Robinho"},
-        {ano:2004, mvp: "Diego Ribas"},
+        {ano:1961, mvp: "Melhor jogador: Pelé"},
+        {ano:1962, mvp: "Melhor jogador: Pelé"},
+        {ano:1963, mvp: "Melhor jogador: Pelé"},
+        {ano:1964, mvp: "Melhor jogador: Pelé"},
+        {ano:1965, mvp: "Melhor jogador: Pelé"},
+        {ano:1968, mvp: "Melhor jogador: Pelé"},
+        {ano:2002, mvp: "Melhor jogador: Robinho"},
+        {ano:2004, mvp: "Melhor jogador: Diego Ribas"},
       ]
     },
     copa: {
       nome: "Copa do Brasil",
       img: "./img/copadobr.png",
       conquistas: [
-        {ano:2010, mvp: "Neymar Jr"},
+        {ano:2010, mvp: "Melhor jogador: Neymar Jr"},
       ]
     }
 }
 window.addEventListener('scroll', function(){
 if (window.scrollY > 500) { 
-  navbar.classList.add('scrolled')
+  navbar.classList.add('scrolled');
+  scrollHint.style.display = 'none';
 } else {
-  navbar.classList.remove('scrolled')
+  navbar.classList.remove('scrolled');
 }
 })
+
 
 for (let botao of botoes) {
   botao.addEventListener('click', function(){
@@ -65,9 +68,15 @@ for (let botao of botoes) {
     for (let conquistas of dados.conquistas){
       let item = document.createElement('div');
       item.innerHTML = `
-      <span>${conquistas.ano}</span>
-      <span>${conquistas.mvp}</span> `
+      <span class="conquista-ano">${conquistas.ano}</span>
+      <span class="conquista-mvp">${conquistas.mvp}</span> `
       dadosPainel.appendChild(item);
+      item.classList.add("conquista-itemjs");
+      window.addEventListener('scroll', function(){
+  if (window.scrollY > 500){
+    painel.style.display = 'none'
+  }
+})
     }
   })
 }
